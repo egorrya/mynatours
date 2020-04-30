2a279af9ad53d33737110df7f63c465637b60943
soldsite <fayncore@gmail.com>
2020-04-30 21:53:07 +0700

    app.get added

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from the server", app: "mynatours" });
-});
+app.use(express.json());
 
-app.post("/", (req, res) => {
-  res.send("you can post to this endpoint...");
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "Hello from the server", app: "mynatours" });
+// });
+
+// app.post("/", (req, res) => {
+//   res.send("you can post to this endpoint...");
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.lenght,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 const port = 3000;
