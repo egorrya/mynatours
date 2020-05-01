4f1d09ac94cd91e7fde1f2e8748dd8fa353f3e00
soldsite <fayncore@gmail.com>
2020-05-01 23:53:07 +0700

    serving static files

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express();
 
 // Middlewares
 app.use(morgan("dev"));
-console.log();
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
