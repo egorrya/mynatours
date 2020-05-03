d3db6d702b5c3488cae34c30bf49d87267735e0e
soldsite <fayncore@gmail.com>
2020-05-04 02:36:11 +0700

    refactor to mvc

--- server.js
+++ server.js
@@ -17,37 +17,6 @@ mongoose
   })
   .then(() => console.log("DB connection successful!"));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A tour must have a name"],
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, "A tour must have a price"],
-  },
-});
-
-const Tour = mongoose.model("Tour", tourSchema);
-
-const testTour = new Tour({
-  name: "The Park Hiker",
-  price: 394,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log("errrrrrrrrrrrrrrrrrooooooooooooor", err);
-  });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
