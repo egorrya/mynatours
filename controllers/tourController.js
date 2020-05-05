23ae81f8784b72fc52cdd62ea071d1dbc6a175d5
soldsite <fayncore@gmail.com>
2020-05-06 02:53:45 +0700

    advanced filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,13 +2,29 @@ const Tour = require("./../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
+    // filtering
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lt|lte|)\b/g,
+      (match) => `$${match}`
+    );
 
-    const Tour = await query;
+    // sorting
+    let query = Tour.find(JSON.parse(queryStr));
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("~createdAt");
+    }
+
+    const tours = await query;
 
     res.status(200).json({
       status: "success",
