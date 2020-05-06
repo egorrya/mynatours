65caeabe61569da3d95170fe4e23dc4cf40b116c
soldsite <fayncore@gmail.com>
2020-05-07 00:45:44 +0700

    error refactoring

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 
-const tourRouter = require("./routes/tourRoutes.js");
-const userRouter = require("./routes/userRoutes.js");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
@@ -24,11 +26,11 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find the ${req.originalUrl} on this server`,
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 }); // all() for all http crud methods, * for all
+
+// error handling middleware
+app.use(globalErrorHandler);
 
 // server
 module.exports = app;
