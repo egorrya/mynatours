5c5a3926ffee9b0d1e5edfa5b776ecf249765a6f
soldsite <fayncore@gmail.com>
2020-05-07 03:17:52 +0700

    dev vs prod errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,40 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // operational error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // programming or unknown error: dont leak error
+  } else {
+    // log
+    console.error("ERROR", err);
+
+    // send generic message
+    res.status(500).json({
+      status: "Error",
+      message: "something went wrong",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProd(err, res);
+  }
 };
