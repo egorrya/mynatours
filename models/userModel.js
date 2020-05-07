3e73f373b52bf81de6b101f1be75af9dcc7906e1
soldsite <fayncore@gmail.com>
2020-05-07 18:03:17 +0700

    user creation and password encryption

--- models/userModel.js
+++ models/userModel.js
@@ -1,32 +1,49 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "A tour must have a name"],
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email."],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlenght: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm a password"],
+    validate: {
+      // this is only works with create and save
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: "passwords are not the same",
     },
-    email: {
-      type: String,
-      required: [true, "A tour must have a name"],
-      unique: true,
-      lowercase: true,
-      validate: [validator.isEmail, 'Please provide a valid email.']
-    },
-    photo: type: String,
-    password: {
-      type: String,
-      required: [true, "Please provide a password"],
-      minlenght: 8
-    },
-    passwordConfirm: {
-      type: String,
-      required: [true, "Please confirm a password"]
-    }
-  }
-);
+  },
+});
 
-const User = mongoose.model('User', userSchema);
+// this middleware works between getting the data and putting it into the db
+userSchema.pre("save", function(next) {
+  if (!this.isModified("password")) return next();
+
+  // password ecnryption
+  this.password = bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
